12fd1aa87715e0a76b0d88d82c075c790dd6f6e7
Robert <robert.j.rantanen@helsinki.fi>
2021-07-19 08:37:00 +0300

    my extension to dockerfile

--- dockertest/start.sh
+++ dockertest/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-(curl --silent --retry 30 --retry-delay 1 --retry-connrefused \
+(curl --silent --retry 600 --retry-delay 1 --retry-connrefused \
 http://localhost:8888 ; \
 python3 -m webbrowser http://localhost:8888) &
 
-docker run -p 8888:8888 -v $(pwd):/work jupyter
+docker run -p 8888:8888 -v $(pwd):/work rantro/jupyspark
